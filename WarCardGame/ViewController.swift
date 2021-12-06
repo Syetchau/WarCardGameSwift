f53d974f4c3c231ce0a76382f47e2f63186da000
syetchau95 <syetchau@hotmail.com>
2021-12-06 21:31:19 +0800

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by LiewSyetChau on 6/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
